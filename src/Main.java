945ba42c91c50a9f762b6170d1d91d6ac378a095
AlexDigital20 <avinciguerra@studenti.apuliadigitalmaker.it>
2025-06-18 12:54:48 +0200

    aggiungi-pdf

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
+import Dto.AppuntiDTO;
 import Dto.UtenteDTO;
-import Dto.AppuntiMaterie;
 
 import java.util.Objects;
 
@@ -9,42 +9,20 @@ import java.util.Objects;
 public class Main {
 
 	public static void main (String[] args){
-		UtenteDTO u1 = new UtenteDTO();
-		UtenteDTO u2 = new UtenteDTO("Peppino", "deBellis");
-		UtenteDTO u3 = new UtenteDTO("Peppino", "deBellis");
-		UtenteDTO u4 = new UtenteDTO("Pinco", "pallo");
-
-		System.out.println();
-		System.out.println();
-		System.out.println();
-
-		System.out.println(u1.toString());
-		System.out.println(u2.toString());
-		System.out.println(u3.toString());
-
-		System.out.println();
-
-		System.out.println("u2 e u3 sono uguali? " + u2.equals(u3));
-		System.out.println();
-
-		System.out.println("u1 e u2 sono uguali? " + u1.equals(u3));
-
-		System.out.println();
-
-		System.out.println("u3 e u4 sono uguali? " + u3.equals(u4));
-		System.out.println();
-
-		boolean test = Objects.equals(u3.getId(), u4.getId());
-
-		System.out.println("u3.id e u4.id sono uguali? " + test);
 
 
-		AppuntiMaterie am = new AppuntiMaterie(u2,"filepdf");
-
-		System.out.println("Appunti materie file name= " + am.getNomeFilePDF());
+			AppuntiDTO appunto = new AppuntiDTO( ".pdf");
 
 
 
+			System.out.println("Nome file PDF: " + appunto.getNomeFilePDF());
+
+
+
+
+
+
+
+
+	}
 }
-
-
