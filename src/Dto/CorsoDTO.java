98f03300be506ca673d8812e6e14d7a8ca171065
Pasquale <apuliajava@libero.it>
2025-06-19 12:24:03 +0200

    Update

---
+++ src/Dto/CorsoDTO.java
@@ -0,0 +1,24 @@
+package Dto;
+
+public class CorsoDTO {
+	private String nomeCorso;
+	private int durata;
+
+	public CorsoDTO(String nomeCorso, int durata) {
+		this.nomeCorso = nomeCorso;
+		this.durata = durata;
+	}
+
+	public String getNomeCorso() {
+		return nomeCorso;
+	}
+
+	public int getDurata() {
+		return durata;
+	}
+
+	@Override
+	public String toString() {
+		return nomeCorso + " (" + durata + " ore)";
+	}
+}
