a1323bab8f5a04c5fb88a4eaab39f687036465f1
Christian <31819134+ChristianGalloso@users.noreply.github.com>
2025-06-22 18:18:33 +0200

    Created GestioneUtenti class and edited Main

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
-import DB.GestioneCorso;
+import DB.GestioneCorsi;
+import DB.GestioneUtenti;
 import Dto.AccountDTO;
 import Dto.CorsoDTO;
 import Dto.UtenteDTO;
@@ -7,15 +8,26 @@ public class Main {
 
 	public static void main (String[] args)
 	{
-		UtenteDTO u1=new UtenteDTO("Mario", "Rossi");
-		UtenteDTO u2=new UtenteDTO("Luigi", "Verdi");
-		UtenteDTO u3=new UtenteDTO("Rosalinda", "Haltmann");
+		//CREO 3 NUOVI UTENTI
+		UtenteDTO u1= GestioneUtenti.aggNuovoUtente("Mario", "Rossi");
+		System.out.println("L'utente "+ u1.getNome()+" "+ u1.getCognome()+" è stato creato correttamente");
+		UtenteDTO u2= GestioneUtenti.aggNuovoUtente("Luigi", "Verdi");
+		System.out.println("L'utente "+ u2.getNome()+" "+ u2.getCognome()+" è stato creato correttamente");
+		UtenteDTO u3=GestioneUtenti.aggNuovoUtente("Rosalinda", "Galaxy");
+		System.out.println("L'utente "+ u3.getNome()+" "+ u3.getCognome()+" è stato creato correttamente");
+
+		//CREO I 3 ACCOUNT DEI 3 UTENTI
 		AccountDTO a1=new AccountDTO("Nintendo","MammaMia","mario@gmail.com","0", u1);
+		System.out.println("L'account per l'utente "+ u1.getNome()+" "+ u1.getCognome()+" è stato creato correttamente");
 		AccountDTO a2=new AccountDTO("Nintendo2","MammaMia2","luigi@gmail.com","1", u2);
+		System.out.println("L'account per l'utente "+ u2.getNome()+" "+ u2.getCognome()+" è stato creato correttamente");
 		AccountDTO a3=new AccountDTO("Galaxy","Luma69","galaxy@gmail.com","2", u3);
+		System.out.println("L'account per l'utente "+ u3.getNome()+" "+ u3.getCognome()+" è stato creato correttamente");
+
+
 		System.out.println(a1.login(u1));
 		System.out.println("\nAggiungo un nuovo corso:");
-		boolean aggiunto = GestioneCorso.aggiungiCorso(new CorsoDTO("Informatica Generale", 15));
+		boolean aggiunto = GestioneCorsi.aggiungiCorso(new CorsoDTO("Informatica Generale", 15));
 		System.out.println(aggiunto ? "Corso aggiunto!" : "Corso già presente.");
 		System.out.println(a1.logout(u1));
 		System.out.println(a2.login(u2));
